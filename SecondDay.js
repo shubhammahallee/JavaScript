7919989509c9367819de1a9413728283c5e17279
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-14 17:30:54 +0530

    Update SecondDay.js

--- SecondDay.js
+++ SecondDay.js
@@ -1,1 +1,27 @@
+let a = 5, b = 7, c = 9;
+if (a < b) {
+    console.log("b is greater than a");
+}
+else if (b < c) {
+    console.log("c is greater than b");
+}
+else {
+    console.log("a is greater than c");
+}
 
+
+let food = "masta";
+
+switch (food) {
+    case "pizza":
+        console.log("pizza");
+        break;
+    case "burger":
+        console.log("burger");
+        break;
+    case "pasta":
+        console.log("pasta");
+        break;
+    default:
+        console.log("other food not in the menu");
+}
