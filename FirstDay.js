de98a751837903f34c5d739c5bf0ddfb5534e452
shubhammahallee <shubhammahallee@gmail.com>
2026-08-14 11:30:40 +0530

    Add JavaScript variables

--- FirstDay.js
+++ FirstDay.js
@@ -1,4 +1,82 @@
+process.stdout.write("Hello javascript\n");
 console.log("Hello World");
-console.log('I love Urvi', 19);
+console.log('Urvi', 19);
 console.log('1' + '1');
 console.log('ur' + 'vi');
+
+
+// ==================== VARIABLES ====================
+
+// Variable Declaration
+
+let name = "Shubham";
+const age = 21;
+var city = "Pune";
+console.log(name, age, city);
+
+
+// ==================== DATA TYPES ====================
+
+// String
+// Number
+// Boolean
+// Undefined
+// Null
+// Object
+// Array
+
+
+// ==================== OPERATORS ====================
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+
+
+// ==================== CONDITIONAL STATEMENTS ====================
+
+// if
+// else if
+// else
+// switch
+
+
+// ==================== LOOPS ====================
+
+// for
+// while
+// do while
+// for...of
+// for...in
+
+
+// ==================== FUNCTIONS ====================
+
+
+// ==================== ARRAYS ====================
+
+
+// ==================== OBJECTS ====================
+
+
+// ==================== STRINGS ====================
+
+
+// ==================== ES6 FEATURES ====================
+
+
+// ==================== ASYNC JAVASCRIPT ====================
+
+// Promise
+// async
+// await
+
+
+// ==================== DOM ====================
+
+
+// ==================== CLASSES ====================
+
+
+// ==================== ERROR HANDLING ====================
