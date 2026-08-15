d7ad7b3b40c7d515030e1f93d6458eaa9b2df434
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-15 13:21:18 +0530

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,1 +1,27 @@
+// ==================== FUNCTIONS ====================
 
+// Function without parameters and return value
+
+function greet() {
+    console.log("Hello");
+}
+
+greet();
+
+
+// Function with parameters and return value
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+
+// Function with parameters but without return value
+
+function addNumbers(a, b) {
+    console.log(a + b);
+}
+
+addNumbers(3, 6);
