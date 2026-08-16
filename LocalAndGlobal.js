e6d0910136c795baf06c790a7edd77a2e0ee7471
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-16 09:10:08 +0530

    Update LocalAndGlobal.js

--- LocalAndGlobal.js
+++ LocalAndGlobal.js
@@ -1,1 +1,34 @@
+// ==================== GLOBAL & LOCAL SCOPE ====================
 
+// Global variables
+let x = 5;
+let y = 10;
+
+
+// Local variable inside function
+function demonstrateLocalScope() {
+    let z = 11;
+
+    // Global variable can be accessed inside the function
+    console.log(x);
+}
+
+demonstrateLocalScope();
+
+
+// Local variable belongs only to its function
+function demonstrateLocalVariable() {
+    let z = 12;
+
+    console.log(z);
+}
+
+demonstrateLocalVariable();
+
+
+// Global variables are accessible inside different functions
+function demonstrateGlobalScope() {
+    console.log(x, y);
+}
+
+demonstrateGlobalScope();
