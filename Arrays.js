7b2e181b53a62f5c5ea8128f559020103d9f9a2d
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-17 11:43:29 +0530

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -5,7 +5,7 @@ let arr = [1, 2, 3, 4, 5];
 console.log(arr);
 console.log(arr.length);
 
-console.log(arr[0]); // Access element using index
+console.log(arr[0]); // Access element using index 
 console.log(arr[1]);
 console.log(arr[2]);
 console.log(arr[3]);
