00b1391800db82049ba8256c4222f60d2f976068
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-19 08:11:30 +0530

    Update AsynchronousJavaScript.js

--- AsynchronousJavaScript.js
+++ AsynchronousJavaScript.js
@@ -1,1 +1,36 @@
+function sendusername() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Urvi");
+            resolve();
+        }, 1000);
+    });
+}
 
+function sendpassword() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Urvi123");
+            resolve();
+        }, 1000);
+    });
+}
+
+function click() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Click");
+            resolve();
+        }, 1000);
+    });
+}
+
+async function main() {
+
+    await sendusername();
+    await sendpassword();
+    await click();
+
+}
+
+main();
